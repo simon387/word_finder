321aa353c2dc0c93762a05147dc5c8e8d812c2f2
simone.celia <simone.celia@eng.it>
2024-02-29 10:21:48 +0100

    sistema

--- src/main/java/it/wordfinder/WordFinder.java
+++ src/main/java/it/wordfinder/WordFinder.java
@@ -16,9 +16,9 @@ public final class WordFinder {
 	private static final String DICTIONARY_FILE_PATH = "src/main/resources/dictionary.txt";
 
 	public static void main ( String[] args ) {
-		var wordLength = 8;
-		var initialChars = "r".toLowerCase ();
-		var onlyPossibleChars = "ounreesuepoabmcrera".toLowerCase (); // but not all mandatories // copy the exaxct values from g
+		var wordLength = 7;
+		var initialChars = "s".toLowerCase ();
+		var onlyPossibleChars = "amssreicten".toLowerCase (); // but not all mandatories // copy the exaxct values from g
 		var lastChars = "".toLowerCase ();
 		formatOutput ( findWords ( wordLength, initialChars, onlyPossibleChars, lastChars ) );
 	}

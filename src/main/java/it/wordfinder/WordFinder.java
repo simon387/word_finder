ebd03bdfca86a29ae41051338c1ec9fd02aab2e2
Simone Celia <simone.celia@simonecelia.it>
2024-05-09 10:23:51 +0200

    scopa

--- src/main/java/it/wordfinder/WordFinder.java
+++ src/main/java/it/wordfinder/WordFinder.java
@@ -17,9 +17,9 @@ public final class WordFinder {
 
 	@SuppressWarnings ( "all" )
 	public static void main ( String[] args ) {
-		Integer wordLength = 7;
-		var initialChars = "a".toLowerCase ();
-		var onlyPossibleChars = "llhaacuneictad".toLowerCase (); // but not all mandatories // copy the exaxct values from g
+		Integer wordLength = 5;
+		var initialChars = "s".toLowerCase ();
+		var onlyPossibleChars = "epcanpsoaoa".toLowerCase (); // but not all mandatories // copy the exaxct values from g
 		var lastChars = "".toLowerCase ();
 		formatOutput ( findWords ( wordLength, initialChars, onlyPossibleChars, lastChars ) );
 	}
